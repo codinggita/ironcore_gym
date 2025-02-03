77ebd4f5291e6cf25ae7f28bc59afc57a4e5d41b
Ridham2808 <ridham.patel.cg@gmail.com>
2025-02-03 19:35:43 +0530

    new commit

--- Backend/User-Auth/server.js
+++ Backend/User-Auth/server.js
@@ -16,6 +16,8 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: "https://authentication-qi2o.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
