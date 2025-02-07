ab6f5b259d44de879c1e98ea9ec0e4051c310256
Ridham2808 <ridham.patel.cg@gmail.com>
2025-02-07 13:36:23 +0530

    fix error.

--- Frontend/vite_app/src/Authentication/login.jsx
+++ Frontend/vite_app/src/Authentication/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import backgroundImage from "../assets/login.png";
+import backgroundImage from "../assets/Login.png";
 
 function Login() {
   const [formData, setFormData] = useState({ email: "", password: "" });
