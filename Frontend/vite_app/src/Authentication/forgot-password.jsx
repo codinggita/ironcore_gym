9ffe6235f8cf4790060468f592eb706548a95e89
Ridham2808 <ridham.patel.cg@gmail.com>
2025-02-24 20:05:11 +0530

    Add link.

--- Frontend/vite_app/src/Authentication/forgot-password.jsx
+++ Frontend/vite_app/src/Authentication/forgot-password.jsx
@@ -19,6 +19,7 @@ function ForgotPassword() {
     
     try {
       const response = await fetch("https://authentication-backend-kbui.onrender.com/api/user/forgot-password", {
+      // const response = await fetch("http://localhost:5000/api/user/forgot-password", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
